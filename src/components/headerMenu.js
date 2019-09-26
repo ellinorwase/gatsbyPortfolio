017fddde4686ab4ed82136155142157550b3105c
Ellinore <ellinor.wase@gmail.com>
2019-09-26 17:39:07 +0200

    menu-700px

--- src/components/headerMenu.js
+++ src/components/headerMenu.js
@@ -2,10 +2,11 @@ import React from "react"
 import Meny from "./menu"
 
 export default () => (
-
+  <div className="slide">
         <nav id="slide">
           <ul className="menu" style={{ textDecoration: `none` }} id="navbar">
           <Meny/>
           </ul>
         </nav>
+  </div>
 )
