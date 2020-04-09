178807210bbe1fa582fc7690161f21fe7e1478c2
Christopher Af Bjur <chrisdevcabc@gmail.com>
2020-04-09 11:07:18 +0200

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,49 +3,58 @@
  *
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
-console.log(`HÄR:  ${__dirname}/src/images` );
+console.log(`HÄR:  ${__dirname}/src/images`)
 
 module.exports = {
   /* Your site config here */
-    plugins: [
-      // Typography
-        {
-          resolve: `gatsby-plugin-typography`,
-          options: {
-            pathToConfigModule: `src/utils/typography`,
-          },
-        },
-        // Image
-        {
-          resolve: `gatsby-source-filesystem`,
-          options: {
-            name: `images`,
-            path: `${__dirname}/src/images`,
-          },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        // Google Analytict
-        {
-          resolve: `gatsby-plugin-google-analytics`,
-          options: {     
-            trackingId: "UA-146676054-1"
-          }
-        },
-        //SASS
-        `gatsby-plugin-sass`,
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/icon.png`,
+      },
+    },
+    // Typography
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    // Image
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // Google Analytict
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-146676054-1",
+      },
+    },
+    //SASS
+    `gatsby-plugin-sass`,
 
-        // SVG-pics
-        // {
-        //   resolve: "gatsby-plugin-react-svg",
-        //   options: {
-        //     rule: {
-        //       include: /assets/ // See below to configure properly
-        //     }
-        //   }
-        // }
-      ],
-      
-      
-  
+    // SVG-pics
+    // {
+    //   resolve: "gatsby-plugin-react-svg",
+    //   options: {
+    //     rule: {
+    //       include: /assets/ // See below to configure properly
+    //     }
+    //   }
+    // }
+  ],
 }
